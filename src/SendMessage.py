0fb099030f87816aca12437a478a157f0e59a7d1
MarSHub <madlsh@icloud.com>
2023-03-14 22:08:25 -0600

    Message-inclusion

--- src/SendMessage.py
+++ src/SendMessage.py
@@ -2,8 +2,8 @@ from twilio.rest import Client
 
 
 def sendWhatsApp(message_string):
-    account_sid = 'AC17d5d8f84a6367c55cb58d7148853294'
-    auth_token = '3a993765a2c079938c6fc2cff5b2e448'
+    account_sid = ''
+    auth_token = ''
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
